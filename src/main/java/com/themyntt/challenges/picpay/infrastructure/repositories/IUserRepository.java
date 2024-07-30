7907f2684e988be4c90236e8e689efb74e73a998
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-30 17:44:04 -0300

    chore: findByEmailAndPassword method

--- src/main/java/com/themyntt/challenges/picpay/infrastructure/repositories/IUserRepository.java
+++ src/main/java/com/themyntt/challenges/picpay/infrastructure/repositories/IUserRepository.java
@@ -4,6 +4,9 @@ import com.themyntt.challenges.picpay.infrastructure.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmailAndPassword(String email, String password);
 }
