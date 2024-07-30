9a6f7471b8ed5d26ca4a14a8da6f67022a7a82e2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-30 17:05:20 -0300

    fix: @repository annotation at top of interface

--- src/main/java/com/themyntt/challenges/picpay/infrastructure/repositories/IUserRepository.java
+++ src/main/java/com/themyntt/challenges/picpay/infrastructure/repositories/IUserRepository.java
@@ -2,6 +2,8 @@ package com.themyntt.challenges.picpay.infrastructure.repositories;
 
 import com.themyntt.challenges.picpay.infrastructure.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
 }
